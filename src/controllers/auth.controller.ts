472fe8f32194fcd37c945aa59ccb31c7502d3d93
Santiago Ayala <santitomayala07@gmail.com>
2026-01-22 10:04:51 -0300

    Controlador de auth basico terminado

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,58 @@
+import type { Request, Response } from "express";
+
+import { login, register } from "../services/authService.js";
+
+//* Controlador de registro
+export const registerController = async (req: Request, res: Response) => {
+  try {
+    const { username, email, password } = req.body;
+    const userRegister = await register({ username, email, password });
+    if (userRegister) {
+      return res.status(201).json({
+        ok: true,
+        msg: "Usuario creado correctamente",
+        data: userRegister,
+      });
+    }
+  } catch (err) {
+    console.error("error", err);
+    return res.status(500).json({
+      ok: false,
+      msg: "Server error",
+    });
+  }
+};
+
+//* Controlador de login
+export const loginController = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    const userLogged = await login({ email, password });
+
+    if (userLogged) {
+      return res.status(200).json({
+        ok: true,
+        msg: "Login exitoso",
+        token: userLogged,
+      });
+    }
+  } catch (err: any) {
+    if (
+      err.message === "Credenciales incorrectas" ||
+      err.message === "Credenciales invalidas"
+    ) {
+      //TODO Esto deberia ir en un middleware
+      return res.status(401).json({
+        ok: false,
+        msg: "Email o contraseña incorrectos",
+      });
+    }
+
+    console.error(err);
+    return res.status(500).json({
+      ok: false,
+      msg: "Error interno del servidor",
+    });
+  }
+};
