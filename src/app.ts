b43be17529f41c986d9669656d9e972b5c171c0c
Santiago Ayala <santitomayala07@gmail.com>
2026-01-22 10:02:22 -0300

    Modificacion para que el sistema lea json

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
 import express from "express";
 import { type Request, type Response } from "express";
 import "dotenv/config";
+import router from "./routes/index.js";
 
 const app = express();
+app.use(express.json());
+
 const PORT = process.env["PORT"];
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Servidor levantado paa");
 });
+
+app.use("/api", router);
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto http://localhost:${PORT}`);
